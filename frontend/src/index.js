8721fe6e4128cae698a284821dcc38e47f0242a7
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-05-25 09:08:07 -0700

    continued integration of redux to manage application state of generation and dragons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,41 +1,32 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import Generation from './components/Generation';
 import Dragon from './components/Dragon';
+import thunk from 'redux-thunk';
+
+
 import './index.css';
-import { createStore } from 'redux';
-
-
-const DEFAULT_GENERATION = { generationId: '', expiration: ' ' };
-const generationReducer = (state, action) => {
-
-  console.log('generationReducer', state);
-
-  if (action.type === 'GENERATION_ACTION_TYPE') {
-    return { generation: action.generation }
-  }
-
-  return {
-    generation: DEFAULT_GENERATION
-  }
-}
-
-const store = createStore(generationReducer);
-
-store.dispatch({
-  type: 'GENERATION_ACTION_TYPE',
-  generation: { generationId: 'goo', expiration: 'hello' }
-});
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import { generationActionCreator } from './actions/generation';
 
 
 
-console.log('store.getState()', store.getState())
+const store = createStore(
+  rootReducer, /* preloadedState, */
+  +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
+
 
 render(
-  <div>
-    <h2>Dragon Stack</h2>
-    <Generation />
-    <Dragon />
-  </div>,
+  <Provider store={store}>
+    <div>
+      <h2>Dragon Stack</h2>
+      <Generation />
+      <Dragon />
+    </div>
+  </Provider>,
   document.getElementById('root')
 );
